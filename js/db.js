60699e1d479d5349ef0b7098bccee8a126d41f7c
Facundo Vera <facundovera.dev@gmail.com>
2025-11-25 16:57:12 -0300

    En esta etapa voy a implementar renderizado dinámico de tarjetas con JSON

---
+++ js/db.js
@@ -0,0 +1,191 @@
+const datosRecursos =[
+  {
+    "categoria": "Animaciones",
+    "recursos": [
+      {
+        "titulo": "SVG Arista",
+        "url_destino": "https://svgartista.netlify.app/",
+        "url_imagen": "https://d25ltszcjeom5i.cloudfront.net/329462/pmtulcbxab/svg-arista-fotor-202509051807.png"
+      },
+      {
+        "titulo": "CSS Loaders",
+        "url_destino": "https://css-loaders.com/",
+        "url_imagen": "https://d25ltszcjeom5i.cloudfront.net/329462/fwwdveakyz/ss-loaders-fotor-2025090518030.png"
+      },
+      {
+        "titulo": "Hover.css",
+        "url_destino": "https://ianlunn.github.io/Hover/"
+      },
+      {
+        "titulo": "Animista",
+        "url_destino": "https://animista.net/play/basic/rotate-scale"
+      },
+      {
+        "titulo": "Plantillas HTML",
+        "url_destino": "https://plantillashtmlgratis.com/categoria/efectos-css/animaciones-css/",
+        "url_imagen": "https://d25ltszcjeom5i.cloudfront.net/329462/tbouiipled/plantillas-html-fotor-2025090518158.png"
+      },
+      {
+        "titulo": "FreeFrontend",
+        "url_destino": "https://freefrontend.com/css-animations/"
+      }
+    ]
+  },
+  {
+    "categoria": "Color",
+    "recursos": [
+      {
+        "titulo": "Coolors",
+        "url_destino": "https://coolors.co/"
+      },
+      {
+        "titulo": "Color Hunt",
+        "url_destino": "https://colorhunt.co/"
+      },
+      {
+        "titulo": "Adobe Color",
+        "url_destino": "https://color.adobe.com/es/create/color-wheel"
+      },
+      {
+        "titulo": "Paletton",
+        "url_destino": "https://paletton.com/#uid=13m0u0kukgalkoupCkzDebmOV6t",
+        "url_imagen": "https://d25ltszcjeom5i.cloudfront.net/329462/xzcybcneyr/palleton-fotor-2025090518326.png"
+      },
+      {
+        "titulo": "Happy Hues",
+        "url_destino": "https://www.happyhues.co/palettes/17"
+      },
+      {
+        "titulo": "Colormind",
+        "url_destino": "http://colormind.io/"
+      }
+    ]
+  },
+  {
+    "categoria": "Fotos",
+    "recursos": [
+      {
+        "titulo": "Pexels (Fotos)",
+        "url_destino": "https://www.pexels.com/es-es/"
+      },
+      {
+        "titulo": "Pixabay (Fotos)",
+        "url_destino": "https://pixabay.com/es/users/pexels-2286921/"
+      },
+      {
+        "titulo": "Unsplash",
+        "url_destino": "https://unsplash.com/es/",
+        "url_imagen": "https://d25ltszcjeom5i.cloudfront.net/329462/ufbqygknbt/unsplash-fotor-20250905175948.png"
+      },
+      {
+        "titulo": "Splitshire",
+        "url_destino": "https://www.splitshire.com/",
+        "url_imagen": "https://d25ltszcjeom5i.cloudfront.net/329462/dqydeahauv/split-fotor-2025090518055.png"
+      },
+      {
+        "titulo": "Shotstash",
+        "url_destino": "https://shotstash.com/",
+        "url_imagen": "https://d25ltszcjeom5i.cloudfront.net/329462/jqawdmjlla/shotstash2-fotor-20250905185043.png"
+      },
+      {
+        "titulo": "Shopify Stock",
+        "url_destino": "https://shopify.com/stock",
+        "url_imagen": "https://d25ltszcjeom5i.cloudfront.net/329462/rdikzckwjj/shopify-fotor-2025090517572.png"
+      }
+    ]
+  },
+  {
+    "categoria": "Fuentes",
+    "recursos": [
+      {
+        "titulo": "DaFont",
+        "url_destino": "https://www.dafont.com/es/"
+      },
+      {
+        "titulo": "Google Fonts",
+        "url_destino": "https://fonts.google.com/"
+      },
+      {
+        "titulo": "Font Squirrel",
+        "url_destino": "https://www.fontsquirrel.com/"
+      },
+      {
+        "titulo": "Befonts",
+        "url_destino": "https://befonts.com/"
+      },
+      {
+        "titulo": "1001 Fonts",
+        "url_destino": "https://www.1001fonts.com/"
+      },
+      {
+        "titulo": "Urbanfonts",
+        "url_destino": "https://www.urbanfonts.com/",
+        "url_imagen": "https://d25ltszcjeom5i.cloudfront.net/329462/ljvhkyfxla/urban-fonts-fotor-20250905175922.png"
+      }
+    ]
+  },
+  {
+    "categoria": "Iconos",
+    "recursos": [
+      {
+        "titulo": "Icons8",
+        "url_destino": "https://iconos8.es/icons",
+        "url_imagen": "https://d25ltszcjeom5i.cloudfront.net/329462/fwujrnfrmb/icons-fotor-2025090518453.png"
+      },
+      {
+        "titulo": "Flaticon",
+        "url_destino": "https://www.flaticon.com/"
+      },
+      {
+        "titulo": "Iconfinder",
+        "url_destino": "https://www.iconfinder.com/",
+        "url_imagen": "https://d25ltszcjeom5i.cloudfront.net/329462/mvfakflqhh/icon-finder-fotor-2025090518515.png"
+      },
+      {
+        "titulo": "Material Icons (Google)",
+        "url_destino": "https://fonts.google.com/icons",
+        "url_imagen": "https://d25ltszcjeom5i.cloudfront.net/329462/euahejrkyn/material-icons-fotor-2025090518413.png"
+      },
+      {
+        "titulo": "Bootstrap Icons",
+        "url_destino": "https://icons.getbootstrap.com/"
+      },
+      {
+        "titulo": "Heroicons",
+        "url_destino": "https://heroicons.com/"
+      }
+    ]
+  },
+  {
+    "categoria": "Video",
+    "recursos": [
+      {
+        "titulo": "Pixabay Videos",
+        "url_destino": "https://pixabay.com/videos/",
+        "url_imagen": "https://d25ltszcjeom5i.cloudfront.net/329462/mkioqfaabc/pixabay-video-fotor-2025090518223.png"
+      },
+      {
+        "titulo": "Pexels (Videos)",
+        "url_destino": "https://www.pexels.com/videos/",
+        "url_imagen": "https://d25ltszcjeom5i.cloudfront.net/329462/hizqikqjkz/pexels-video-fotor-202509051835.png"
+      },
+      {
+        "titulo": "Mixkit",
+        "url_destino": "https://mixkit.co/free-stock-video/",
+        "url_imagen": "https://d25ltszcjeom5i.cloudfront.net/329462/kyzbmhiniq/mix-video-fotor-2025090518354.png"
+      },
+      {
+        "titulo": "Covver",
+        "url_destino": "https://coverr.co"
+      },
+      {
+        "titulo": "Freepik (Videos)",
+        "url_destino": "https://www.freepik.com/"
+      },
+      {
+        "titulo": "Vidsplay",
+        "url_destino": "https://www.vidsplay.com/"
+      }
+    ]
+  }
+]
